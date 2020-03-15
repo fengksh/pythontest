589e3669ddd70d33b577dc48d1738087bb3c6862
fengksh <1034937014@qq.com>
2020-03-15 17:35:53 +0800

    新修改的测试文件

--- testpython/test420200314.py
+++ testpython/test420200314.py
@@ -8,9 +8,28 @@ os.open 打开有问题，暂时不明白如何jie
 '''
 print(os.getcwd())
 file = open(r'E:/work/python/testpython/chapter3/sketch.txt')
-print(file.read(),end="")
+#print(file.read(),end="")
 file.close()
 '''
 文件关闭后无法在读取文件
 '''
 #print(file.read(),end="")
+try:
+    file = open(r'E:/work/python/testpython/chapter3/sketch.txt')
+except:
+    pass
+
+'''
+打印两行会一词打印
+'''
+#print(file.readline(),end="")
+#print(file.readline(),end="")
+#file.close()
+for each_list in file:
+    try:
+        (role,line_spoken) = each_list.split(":",1)
+        print(role,end="")
+        print(" said: ",end="")
+        print(line_spoken,end="")
+    except:
+        pass
